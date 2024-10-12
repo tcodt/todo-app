2dd17fbe7f882cc929d8b199946f2e5944f19f7e
TCOD <amirprogrammer38@gmail.com>
2024-10-12 13:13:00 +0330

    add HTML structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="task-container">
+        <h1>TODO APP ✏</h1>
+        <div className="task-box">
+          <div className="input-box">
+            <input type="text" />
+            <button>Add</button>
+          </div>
+          <ul className="task-list">{/* TODOS */}</ul>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
